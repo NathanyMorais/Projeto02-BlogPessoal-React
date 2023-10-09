33c732afbf08e48a11d4b8ca1eb208220401f4c1
Nathany <nathanyk.morais@gmail.com>
2023-10-09 17:31:10 -0300

    criação do componente Perfil

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -15,25 +15,29 @@ function Navbar() {
 
   let navbarComponent  //variavel usada no conteúdo sobre renderização condicional
   
-  return (
-    <>
-     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
+  if(usuario.token !== "") {
+    navbarComponent = (
+      <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+        <div className="container flex justify-between text-lg">
           <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
 
-            <div className='flex gap-4'>
-              
+          <div className='flex gap-4'>
             <Link to='/postagens' className='hover:underline'>Postagens</Link>
             <Link to='/temas' className='hover:underline'>Temas</Link>
             <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
-              <div className='hover:underline'>Perfil</div>
-              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
-             
-            </div>
+            <Link to='/perfil' className='hover:underline'>Perfil</Link>
+            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
           </div>
         </div>
-    </>
-  )
+      </div>
+    )
+  }
+
+return (
+  <>
+    {navbarComponent}
+  </>
+)
 }
 
 export default Navbar
